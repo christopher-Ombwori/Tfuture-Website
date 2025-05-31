27d5ba46fc9e0ed87ec570128990d01c1034f6ac
christopher-Ombwori <einsteinchris402@gmail.com>
2025-05-31 13:21:04 +0300

    added more allowed hosts

--- TFuture/settings.py
+++ TFuture/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3$@oryvqkf5gk!x*7cl87
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1', 'tfuturedesigns.studio', 'https://tfuture-website.onrender.com').split(',')
 
 
 # Application definition
